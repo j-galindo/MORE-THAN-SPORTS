3a52ee158dbadc5ce0d9bf3c66acc9052a1fce46
Jose Galindo <jgalindo.ironhack@gamil.com>
2019-08-27 13:43:55 -0400

    working

--- main.js
+++ main.js
@@ -21,10 +21,36 @@ $("#button").click(function() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.querySelector("#topstories").onclick = function() {
     axios.get(`https://api.nytimes.com/svc/topstories/v2/sports.json?api-key=OojsGeEqutJg4lecTcp2iLYiSHsBZx5r`).then((articles) => {
         console.log(articles.data)
-        topstoryHTML = ` 
+        topstoryHTML = `
         <div class="card-body">
             <h5 class="card-title">(1)Top Story</h5>
             <p class="card-text">
@@ -32,7 +58,7 @@ document.querySelector("#topstories").onclick = function() {
             </p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
-        <img src="${articles.data.results[2].multimedia[4].url}" class="card-img-top" alt="...">
+        <img id ="image" src="${articles.data.results[2].multimedia[4].url}" class="card-img-top" alt="...">
 
         <div class="card-body">
             <h5 class="card-title">(2)Top Story</h5>
@@ -41,7 +67,7 @@ document.querySelector("#topstories").onclick = function() {
             </p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
-        <img src="${articles.data.results[6].multimedia[4].url}" class="card-img-top" alt="...">
+        <img id ="image" src="${articles.data.results[6].multimedia[4].url}" class="card-img-top" alt="...">
 
         <div class="card-body">
             <h5 class="card-title">(3)Top Story</h5>
@@ -50,7 +76,7 @@ document.querySelector("#topstories").onclick = function() {
             </p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
-        <img src="${articles.data.results[9].multimedia[4].url}" class="card-img-top" alt="...">
+        <img id ="image" src="${articles.data.results[9].multimedia[4].url}" class="card-img-top" alt="...">
 
 
         <div class="card-body">
@@ -60,7 +86,7 @@ document.querySelector("#topstories").onclick = function() {
             </p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
-        <img src="${articles.data.results[12].multimedia[4].url}" class="card-img-top" alt="...">
+        <img id ="image" src="${articles.data.results[12].multimedia[4].url}" class="card-img-top" alt="...">
 
 
         <div class="card-body">
@@ -70,7 +96,7 @@ document.querySelector("#topstories").onclick = function() {
             </p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
-        <img src="${articles.data.results[5].multimedia[4].url}" class="card-img-top" alt="...">
+        <img id ="image" src="${articles.data.results[5].multimedia[4].url}" class="card-img-top" alt="...">
 
         <div class="card-body">
             <h5 class="card-title">(6)Top Story</h5>
@@ -79,7 +105,7 @@ document.querySelector("#topstories").onclick = function() {
             </p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
-        <img src="${articles.data.results[17].multimedia[4].url}" class="card-img-top" alt="...">`
+        <img id ="image" src="${articles.data.results[17].multimedia[4].url}" class="card-img-top" alt="...">`
         document.querySelector('.card').innerHTML = topstoryHTML
     })
 }
@@ -88,18 +114,18 @@ document.querySelector("#topstories").onclick = function() {
 document.querySelector("#button").onclick = function() {
     axios.get(`https://api.nytimes.com/svc/topstories/v2/sports.json?api-key=OojsGeEqutJg4lecTcp2iLYiSHsBZx5r`).then((articles) => {
 
-        console.log(articles.data.results)
+        let array = articles.data.results.title;
 
         let input = document.querySelector("#navbarSupportedContent > form > input").value;
-        // if (input > 19) {
+        if (input > 19) {
 
-        //     alert("Please Enter a number of 1-19")
-        // }
+            alert("Please Enter a number of 1-19")
+        }
 
         console.log(input)
-        var search = input.filter(function(item) {
-            return item.tile == input;
-        });
+            // var search = input.filter(function(item) {
+            //     return array.tile == input;
+            // });
 
         updateHTML = `<div class="card-body">
         <h5 class="card-title">(1)Top Story</h5>
